9cd7e027013bfa80abfe3bf4061f4d9ba599d6ad
Ladomitory <radomirholmogorov@yandex.ru>
2023-11-18 15:12:24 +0700

    update task 1

--- Seminar 1/Task 1/Program.cs
+++ Seminar 1/Task 1/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.InteropServices;
+using System.Xml;
+
+//boolian
+bool b = true;
+
+//integer
+short s = 16;
+ushort us = 16;
+int i = 32;
+uint ui = 32;
+long l = 64;
+ulong ul = 64;
+
+//with point
+float f;
+double d;
+decimal dec;
+
+//string
+string str;
+
+//operators
+if (i == ui) {}
+else if (i != ui) {}
+else {}
+
+switch (l) {
+    case 64:
+        break;
+    default:
+        break;
+}
+
+while (i == ui) {
+    break;
+}
+
+do {
+    break;
+} while (i == ui);
+
+for (;i == ui;) {
+    break;
+}
