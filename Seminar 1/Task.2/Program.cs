d7c3a738d5a43d01ee8f848dceb28fee3306a39c
Ladomitory <radomirholmogorov@yandex.ru>
2023-11-18 15:35:08 +0700

    update task.2

--- Seminar 1/Task.2/Program.cs
+++ Seminar 1/Task.2/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿// Напишите программу которая на вход получает число, а возвращает его квадврат
+
+//number - получаемое число, result - результат вычислений
+long number, result;
+Console.Write("Your number: ");
+// получение изначального числа
+number = long.Parse(Console.ReadLine());
+//вычисление результата
+result = number * number;
+Console.WriteLine("Rasult: " + result);
