c111b8434c70b2d287db7e397ccb3fdc8e07cfd3
Ladomitory <radomirholmogorov@yandex.ru>
2023-11-18 16:23:19 +0700

    add task.4

---
+++ Seminar 1/Task.4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача №5. Напишите программу, которая на вход принимает одно число (N), 
+// а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.Write("Your number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+// int negativN = -N;
+// while (negativN <= N) {
+//     Console.Write(negativN + " ");
+//     negativN += 1;
+// }
+
+// int negativN = -N;
+// do {
+//     Console.Write(negativN + " ");
+//     negativN += 1;
+// } while (negativN <= N);
+
+for (int negativN = -N; nagativN <= N; negativN += 1) {
+    Console.Write(negativN + " ");
+}
