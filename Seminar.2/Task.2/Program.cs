7e61f67e5d2ddd9e96273d125c0832f035868092
Radomir <radomirholmogorov@yandex.ru>
2023-11-20 17:04:38 +0700

    add & end task.2 from seminar.2

---
+++ Seminar.2/Task.2/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу которая генерирует случайное троехзначное число
+//и вырезает из него среднюю цифру
+//[100, 999]
+
+int CutSecondDigit(int number) {
+    return Convert.ToInt32(
+        Convert.ToString(number/100) //first digit
+        + Convert.ToString(number % 10) //third digit
+        );
+}
+
+int number = new Random().Next(100, 1000);
+Console.Write(number + " -> ");
+int result = CutSecondDigit(number);
+Console.WriteLine(result);
