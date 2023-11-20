a6388968293c2e503a1dda2cffec40db939c36fc
Radomir <radomirholmogorov@yandex.ru>
2023-11-20 16:52:19 +0700

    End task.1

--- Seminar.2/Task.1/Program.cs
+++ Seminar.2/Task.1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу которая выводит случайное число из диапазона [10, 99]
+//и показывает наибольшую цивру этого числа
+
+// Random().Next -> [a, b)
+int MaxDigit(int number) {
+    int firstDigit = number / 10, secondDigit = number % 10;
+    return (firstDigit >= secondDigit) ? (firstDigit) : (secondDigit);
+}
+
+int number = new Random().Next(10, 100);
+Console.Write(number + " -> ");
+int result = MaxDigit(number);
+Console.WriteLine(result);
