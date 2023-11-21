965bfb879dff10029f47423bae36011f517f0b36
Radomir <radomirholmogorov@yandex.ru>
2023-11-21 14:33:42 +0700

    end task.1 from homework.2

--- HomeWork.2/Task.1/Program.cs
+++ HomeWork.2/Task.1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+// Квадрат в ОБЕ стороны.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+string[] numbers = Console.ReadLine().Split(", ");
+int firstNumber = Convert.ToInt32(numbers[0]), secondNumber = Convert.ToInt32(numbers[1]);
+if (firstNumber * firstNumber == secondNumber || firstNumber == secondNumber * secondNumber) {
+    Console.WriteLine("да");
+} else {
+    Console.WriteLine("нет");
+}
