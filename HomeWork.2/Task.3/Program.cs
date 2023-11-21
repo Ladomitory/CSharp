57d46ce60478fa25ec8a74b9bc86cc66a20e180a
Radomir <radomirholmogorov@yandex.ru>
2023-11-21 14:58:27 +0700

    end task.3

--- HomeWork.2/Task.3/Program.cs
+++ HomeWork.2/Task.3/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3: Напишите программу, которая выводит третью с начала
+// цифру заданного числа или сообщает, что третьей цифры
+// нет.
+// 456 => 6
+// 91 => Третьей цифры нет
+
+Console.Write("Your number: ");
+string line = Console.ReadLine();
+int number = Convert.ToInt32(line), lenOfNumber = line.Length;
+if (lenOfNumber < 3) {
+    Console.WriteLine("Третьей цифры нет");
+} else {
+    int divider = 1;
+    for (int index = 0; index < lenOfNumber - 3; index++) {
+        divider *= 10;
+    }
+    int result = (number / divider) % 10;
+    Console.WriteLine(result);
+}
