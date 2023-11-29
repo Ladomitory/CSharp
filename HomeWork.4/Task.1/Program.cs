5eda4009bb034144b04e1ff623c832de5febca9d
Ladomitory <radomirholmogorov@yandex.ru>
2023-11-29 21:46:26 +0700

    Add homework.4 Add task.1 in hw.4

---
+++ HomeWork.4/Task.1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 1: Напишите программу, которая бесконечно запрашивает целые числа с консоли.
+// Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
+
+while (true) {
+    Console.Write("Введите число: ");
+    string? inputLine = Console.ReadLine();
+    if (inputLine == null) {
+        continue;
+    } else if (inputLine[0] == 'q')  {
+        break;
+    } else if (int.TryParse(inputLine, out int result)) {
+        int sumDigits = 0;
+        for (int index = 0; index < inputLine.Length; index++) {
+            sumDigits += inputLine[index] - 48;
+        }
+        if (sumDigits % 2 == 0) {
+            break;
+        }
+    }
+}
