769e8cf56f7758611896bb7b35106899586b7883
Ladomitory <radomirholmogorov@yandex.ru>
2023-11-29 17:57:06 +0700

    Add task.2 in hw.3

---
+++ HomeWork.3/Task.2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 2: Задайте массив на 10 целых чисел. 
+// Напишите программу, которая определяет количество чётных чисел в массиве.
+
+void printArray(int[] array, int size) {
+    Console.Write("Массив: [ ");
+    for (int index = 0; index < size; index++) {
+        Console.Write($"{array[index]} ");
+    }
+    Console.WriteLine("]");
+}
+
+int arraySize = 10;
+int[] array = new int[arraySize];
+for (int index = 0; index < arraySize; index++) {
+    array[index] = new Random().Next(1, 101);
+}
+printArray(array, arraySize);
+
+int soughtNumbersCounter = 0; 
+for (int index = 0; index < arraySize; index++) {
+    if (array[index] % 2 == 0) {
+        soughtNumbersCounter++;
+    }
+}
+
+Console.WriteLine($"{soughtNumbersCounter} чисел в массиве чётны");
