2ec12f6ebdb78e17db87c18bf33648158386012c
Ladomitory <radomirholmogorov@yandex.ru>
2023-11-29 18:09:41 +0700

    Add task.3 in hw.3

---
+++ HomeWork.3/Task.3/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 3: Задайте массив из вещественных чисел с ненулевой дробной частью. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+void printArray(double[] array, int size) {
+    Console.Write("Массив: [");
+    for (int index = 0; index < size; index++) {
+        Console.Write($"{array[index]} ");
+    }
+    Console.WriteLine("]");
+}
+
+while(true) {
+    Console.Write("Введите размер массива: ");
+    string? stringArraySize = Console.ReadLine();
+    if (int.TryParse(stringArraySize, out int parseResult)) {
+        int arraySize = parseResult;
+        double[] array = new double[arraySize];
+        for (int index = 0; index < arraySize; index++) {
+            array[index] = new Random().NextDouble();
+        }
+        printArray(array, arraySize);
+
+        double minimalInArray = array[0], maximalInArray = array[0];
+        for (int index = 0; index < arraySize; index++) {
+            if (array[index] > maximalInArray) {
+                maximalInArray = array[index];
+            } else if (array[index] < minimalInArray) {
+                minimalInArray = array[index];
+            }
+        }
+
+        Console.WriteLine($"Разница между минимальным и максимальным числом в масиве = {maximalInArray - minimalInArray}");
+        break;
+    } else {
+        Console.WriteLine("Не корректный формат размера массива");
+    }
+}
