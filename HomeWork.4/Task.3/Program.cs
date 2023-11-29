e756cee55a6254b5fd7f3805096290d5d3e52702
Ladomitory <radomirholmogorov@yandex.ru>
2023-11-29 22:02:30 +0700

    Add task.3 in hw.4

---
+++ HomeWork.4/Task.3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 3: Напишите программу, которая перевернёт одномерный массив (первый элемент станет последним, второй – предпоследним и т.д.)
+
+void printArray(int[] array) {
+    Console.Write("[ ");
+    for (int index = 0; index < array.Length; index++) {
+        Console.Write($"{array[index]} ");
+    }
+    Console.WriteLine("]");
+}
+
+int arraySize = 10;
+int[] originalArray = new int[arraySize];
+for (int index = 0; index < originalArray.Length; index++) {
+    originalArray[index] = new Random().Next(100, 1000);
+}
+Console.Write("Исходный массив: ");
+printArray(originalArray);
+
+int[] finalArray = new int[originalArray.Length];
+for (int index = 0; index < finalArray.Length; index++) {
+    finalArray[index] = originalArray[(originalArray.Length - 1) - index];
+}
+Console.Write("Итоговый массив: ");
+printArray(finalArray);
