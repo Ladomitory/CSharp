b0f9af5fa0c14a26a983247e540e86aab5b48772
Radomir <radomirholmogorov@yandex.ru>
2023-12-04 16:47:49 +0700

    Add homework.6 Add task.1 in hw.6

---
+++ HomeWork.6/Task.1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 1: 
+// Задайте двумерный массив символов (тип char [,]). 
+// Создать строку из символов этого массива.
+
+char[,] generationNewmatrix() {
+    Random random = new Random();
+    char[,] matrix = new char[random.Next(1, 5), random.Next(1, 5)];
+    for (int row = 0; row < matrix.GetLength(0); row++) {
+        for (int column = 0; column < matrix.GetLength(1); column++) {
+            matrix[row, column] = (char) random.Next(33, 127);
+        }
+    }
+    return matrix;
+}
+
+void printMatrix(char[,] matrix) {
+    Console.Write("[");
+    for (int row = 0; row < matrix.GetLength(0); row++) {
+        Console.Write("\n[ ");
+        for (int column = 0; column < matrix.GetLength(1); column++) {
+            Console.Write($"{matrix[row, column]} ");
+        }
+        Console.Write("]");
+    }
+    Console.WriteLine("\n]");
+}
+
+char[,] charMatrix = generationNewmatrix();
+printMatrix(charMatrix);
+string stringFromCharMatrix = "";
+for (int row = 0; row < charMatrix.GetLength(0); row++) {
+    for (int column = 0; column < charMatrix.GetLength(1); column++) {
+        stringFromCharMatrix += charMatrix[row, column];
+    }
+}
+Console.WriteLine(stringFromCharMatrix);
