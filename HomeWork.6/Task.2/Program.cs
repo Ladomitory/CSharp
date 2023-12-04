0e75e439cd613c7b8abc72145f6a02d2b6b6d370
Radomir <radomirholmogorov@yandex.ru>
2023-12-04 17:06:40 +0700

    Add task.2 in hw.6

---
+++ HomeWork.6/Task.2/Program.cs
@@ -0,0 +1,26 @@
+// Задача 2:
+// Задайте строку, содержащую латинские буквы в обоих регистрах. 
+// Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+string generationString() {
+    Random random = new Random();
+    string newString = "";
+    int stringLength = 10;
+    for (int index = 0; index < stringLength; index++) {
+        newString += (char) random.Next(33, 127);
+    }
+    return newString;
+}
+
+string ToLower(string originString) {
+    string finalString = "";
+    for (int index = 0; index < originString.Length; index++) {
+        finalString += (char) (( originString[index] <= 'Z' && originString[index] >= 'A') ? (originString[index] + 'a' - 'A') : (originString[index]));
+    }
+    return finalString;
+}
+
+string originString = generationString();
+Console.WriteLine(originString);
+string finalString = ToLower(originString);
+Console.WriteLine(finalString);
