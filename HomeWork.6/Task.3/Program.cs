9dde0dbbf5aab653395b0e4867f606bfda48effa
Radomir <radomirholmogorov@yandex.ru>
2023-12-04 17:14:13 +0700

    add task.3 in hw.6

---
+++ HomeWork.6/Task.3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 3: 
+// Задайте произвольную строку. 
+// Выясните, является ли она палиндромом.
+
+string generationString() {
+    Random random = new Random();
+    string newString = "";
+    int stringLength = random.Next(1, 10);
+    for (int index = 0; index < stringLength; index++) {
+        newString += (char) random.Next(33, 127);
+    }
+    return newString;
+}
+
+string originString = generationString();
+bool polindrom = true;
+for (int index = 0; index < originString.Length; index++) {
+    if (originString[index] != originString[originString.Length - 1 - index]) {
+        polindrom = false;
+        break;
+    }
+}
+Console.WriteLine(originString);
+Console.WriteLine((polindrom) ? ("It\'s polindrom") : ("It isn\'t polindrom"));
