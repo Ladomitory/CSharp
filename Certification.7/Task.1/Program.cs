7884680c373f83c3a9c6732c18b16e9f88e22741
Ladomitory <radomirholmogorov@yandex.ru>
2023-12-09 20:06:57 +0700

    Add sert.7 & task.1 in s.7

---
+++ Certification.7/Task.1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 1: Задайте значения M и N.
+// Напишите программу, которая выведет все
+// натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+
+void PrintRange(int start, int final) {
+    Console.Write($"{start} ");
+    if (start < final) {
+        PrintRange(start + 1, final);
+    }
+}
+
+Console.Write("Введите начало промежутка: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите конец промежутка: ");
+int lastNumber = Convert.ToInt32(Console.ReadLine());
+PrintRange(firstNumber, lastNumber);
