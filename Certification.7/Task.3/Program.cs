3bfa2344891f40a9aa4659577839e13a04de6681
Ladomitory <radomirholmogorov@yandex.ru>
2023-12-09 20:33:11 +0700

    Add task.3 in s.7

---
+++ Certification.7/Task.3/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 3: 
+// Задайте произвольный массив.
+// Выведете его элементы, начиная с конца.
+// Использовать рекурсию, не использовать циклы.
+
+int[] fillArray(int[] array, int depth, Random random) {
+    array[depth] = random.Next(1, 10);
+    if (depth + 1 < array.Length) {
+        return fillArray(array, depth + 1, random);
+    } else {
+        return array;
+    }
+}
+
+int[] GenerateArray() {
+    Random random = new Random();
+    int size = random.Next(1, 10);
+    int[] newArray = fillArray(new int[size], 0, random);
+    return newArray;
+}
+
+void ReversePrintArray(int[] array, int depth) {
+    Console.Write($"{array[array.Length - 1 - depth]} ");
+    if (depth + 1 < array.Length) {
+        ReversePrintArray(array, depth + 1);
+    } else {
+        return;
+    }
+}
+
+int[] array = GenerateArray();
+Console.WriteLine(array.Length);
+ReversePrintArray(array, 0);
