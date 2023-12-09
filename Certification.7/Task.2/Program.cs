1d7240548f5ea1720a396eff51f6d6cd6866d219
Ladomitory <radomirholmogorov@yandex.ru>
2023-12-09 20:18:09 +0700

    Add task.2 in s.7

---
+++ Certification.7/Task.2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 2: 
+// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+int AkkermanFunction(int coefficientN, int coefficientM) {
+    if (coefficientN == 0) {
+        return coefficientM + 1;
+    } else if (coefficientM == 0) {
+        return AkkermanFunction(coefficientN - 1, 1);
+    } else {
+        return AkkermanFunction(coefficientN - 1, AkkermanFunction(coefficientN, coefficientM - 1));
+    }
+}
+
+Console.Write("Введите коэффициэнты в формате 'N M': ");
+string[] input = Console.ReadLine().Split(" ");
+Console.WriteLine(AkkermanFunction(Convert.ToInt32(input[0]), Convert.ToInt32(input[1])));
